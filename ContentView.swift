0a355634c157ab0582a11906faad485544fc405e
slackappu <alexveliyathumalil21@gmail.com>
2025-04-11 11:06:00 -0700

    added subtract button

--- ContentView.swift
+++ ContentView.swift
@@ -7,12 +7,16 @@ struct ContentView: View {
     @State var answer = 0
     var body: some View {
         TextField("number1", value: $number1, format: .number)
-        TextField("Enter second number", value: $number2, format: .number)
-        
-        
+        TextField("number2", value: $number2, format: .number)
         Button("add"){
             answer = number1 + number2
         }
+        Button {
+            answer = number1 - number2
+        } label: {
+            Text("subtract")
+        }
+
         Text("\(answer)")
     }
 }
