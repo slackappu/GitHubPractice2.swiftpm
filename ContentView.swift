628d36e123f4e29aa3152b28d877383a359566ba
slackappu <alexveliyathumalil21@gmail.com>
2025-04-11 10:56:47 -0700

    took out starter code and replaced with Textfield tied to variable number 2.

--- ContentView.swift
+++ ContentView.swift
@@ -2,16 +2,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1 = 0
-    
-    
     @State var number2 = 0
-    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Enter second number", value: $number2, format: .number)
     }
 }
